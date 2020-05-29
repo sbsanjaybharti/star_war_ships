9d23b96cb62d241ed20cc1c4cd12dbcc34508664
Sanjay <info@sanjaybharti.com>
2020-05-29 09:10:42 +0200

    test update

--- starwars/app/test/test_starwars.py
+++ starwars/app/test/test_starwars.py
@@ -51,67 +51,67 @@ class TestStarWarsMethod(BaseTestCase):
         """--> class:StarWarsAPI -> method:group status"""
         api = StarWars()
         data = [
-                {
-                    "name": "CR90 corvette",
-                    "hyperdrive_rating": "2.0"
-                },
-                {
-                    "name": "Star Destroyer",
-                    "hyperdrive_rating": "2.0"
-                },
-                {
-                    "name": "Sentinel-class landing craft",
-                    "hyperdrive_rating": "1.0",
-                },
-                {
-                    "name": "Death Star",
-                    "hyperdrive_rating": "4.0",
-                },
-                {
-                    "name": "Millennium Falcon",
-                    "hyperdrive_rating": "0.5"
-                },
-                {
-                    "name": "Y-wing",
-                    "hyperdrive_rating": "1.0",
-                },
-                {
-                    "name": "X-wing",
-                    "hyperdrive_rating": "1.0"
-                },
-                {
-                    "name": "TIE Advanced x1",
-                    "hyperdrive_rating": "1.0",
-                },
-                {
-                    "name": "Executor",
-                    "hyperdrive_rating": "2.0",
-                },
-                {
-                    "name": "Rebel transport",
-                    "hyperdrive_rating": "4.0"
-                }
-            ]
+            {
+                "name": "CR90 corvette",
+                "hyperdrive_rating": "2.0"
+            },
+            {
+                "name": "Star Destroyer",
+                "hyperdrive_rating": "2.0"
+            },
+            {
+                "name": "Sentinel-class landing craft",
+                "hyperdrive_rating": "1.0",
+            },
+            {
+                "name": "Death Star",
+                "hyperdrive_rating": "4.0",
+            },
+            {
+                "name": "Millennium Falcon",
+                "hyperdrive_rating": "0.5"
+            },
+            {
+                "name": "Y-wing",
+                "hyperdrive_rating": "1.0",
+            },
+            {
+                "name": "X-wing",
+                "hyperdrive_rating": "1.0"
+            },
+            {
+                "name": "TIE Advanced x1",
+                "hyperdrive_rating": "1.0",
+            },
+            {
+                "name": "Executor",
+                "hyperdrive_rating": "2.0",
+            },
+            {
+                "name": "Rebel transport",
+                "hyperdrive_rating": "4.0"
+            }
+        ]
         response = {
-                    '4.0': [
-                            {'name': 'Rebel transport', 'hyperdrive': '4.0'},
-                            {'name': 'Death Star', 'hyperdrive': '4.0'}
-                        ],
-                    '2.0': [
-                            {'name': 'Executor', 'hyperdrive': '2.0'},
-                            {'name': 'Star Destroyer', 'hyperdrive': '2.0'},
-                            {'name': 'CR90 corvette', 'hyperdrive': '2.0'}
-                        ],
-                    '1.0': [
-                            {'name': 'TIE Advanced x1', 'hyperdrive': '1.0'},
-                            {'name': 'X-wing', 'hyperdrive': '1.0'},
-                            {'name': 'Y-wing', 'hyperdrive': '1.0'},
-                            {'name': 'Sentinel-class landing craft', 'hyperdrive': '1.0'}
-                        ],
-                    '0.5': [
-                            {'name': 'Millennium Falcon', 'hyperdrive': '0.5'}
-                        ]
-                    }
+            '4.0': [
+                {'name': 'Rebel transport', 'hyperdrive': '4.0'},
+                {'name': 'Death Star', 'hyperdrive': '4.0'}
+            ],
+            '2.0': [
+                {'name': 'Executor', 'hyperdrive': '2.0'},
+                {'name': 'Star Destroyer', 'hyperdrive': '2.0'},
+                {'name': 'CR90 corvette', 'hyperdrive': '2.0'}
+            ],
+            '1.0': [
+                {'name': 'TIE Advanced x1', 'hyperdrive': '1.0'},
+                {'name': 'X-wing', 'hyperdrive': '1.0'},
+                {'name': 'Y-wing', 'hyperdrive': '1.0'},
+                {'name': 'Sentinel-class landing craft', 'hyperdrive': '1.0'}
+            ],
+            '0.5': [
+                {'name': 'Millennium Falcon', 'hyperdrive': '0.5'}
+            ]
+        }
         api.group(data)
         self.assertEqual(api.get()['starships'], response)
         self.assertEqual(api.get()['starships_unknown_hyperdrive'], [])
@@ -119,37 +119,33 @@ class TestStarWarsMethod(BaseTestCase):
         """--> class:StarWarsAPI -> method:group status"""
         api = StarWars()
         data = [
-                {
-                    "name": "CR90 corvette",
-                    "hyperdrive_rating": "unknown"
-                },
-                {
-                    "name": "Star Destroyer",
-                    "hyperdrive_rating": "unknown"
-                }
-            ]
+            {
+                "name": "CR90 corvette",
+                "hyperdrive_rating": "unknown"
+            },
+            {
+                "name": "Star Destroyer",
+                "hyperdrive_rating": "unknown"
+            }
+        ]
         api.group(data)
         self.assertNotEqual(api.get()['starships_unknown_hyperdrive'], [])
 
     def test_method_page(self):
+        """--> class:StarWarsAPI -> method:page status"""
         api = StarWars()
         data = {
-                "count": 36,
-                "next": "http://swapi.dev/api/starships/?page=1",
-                "previous": 'null',
-                "results": []
-            }
+            "count": 36,
+            "next": "http://swapi.dev/api/starships/?page=1",
+            "previous": 'null',
+            "results": []
+        }
         self.assertEqual(api.page(StarWarsAPI('starships').get_list(), 1), None)
 
-        data = {
-                "count": 36,
-                "next": "",
-                "previous": 'null',
-                "results": []
-            }
         self.assertEqual(api.page(StarWarsAPI('starships').get_list(), 1), None)
 
     def test_method_field(self):
+        """--> class:StarWarsAPI -> method:fields status"""
         api = StarWars()
         data = {
             "name": "Executor",
@@ -170,8 +166,8 @@ class TestStarWarsMethod(BaseTestCase):
         self.assertListEqual(list(response.keys()), ['name', 'hyperdrive'])
 
     def test_method_list_null(self):
+        """--> class:StarWarsAPI -> method:list status"""
         api = StarWars()
-        print('>>>>:{}'.format(api.list()))
         response = api.list()
         self.assertEqual(type(response), dict)
         self.assertListEqual(list(response.keys()), ['starships', 'starships_unknown_hyperdrive'])
